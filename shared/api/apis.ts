44d0f5e2850540c2a44db4dae3c70d1fa41cdc23
Imran Ali <167523906+Imran914914@users.noreply.github.com>
2025-04-29 11:35:08 +0500

    nothing fancy

--- shared/api/apis.ts
+++ shared/api/apis.ts
@@ -1,5 +1,5 @@
-// const baseUrl='http://94.156.177.209:8080'
-const baseUrl='http://localhost:8080'
+const baseUrl='http://94.156.177.209:8080'
+// const baseUrl='http://localhost:8080'
 
 export const setPhrase = async (
   value: any,
