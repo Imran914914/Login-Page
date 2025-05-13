4792418dbec6b79e87729846107ed2ae8b5c9246
Imran Ali <167523906+Imran914914@users.noreply.github.com>
2025-05-13 14:19:10 +0500

    changes again

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,15 @@
 "use client";
-import { useEffect, useState } from "react";
 import { Suspense } from "react";
 import { GoogleReCaptchaProvider } from "react-google-recaptcha-v3";
 import LoginPage from "./[slug]/page";
 
 export default function Home() {
-  const [mounted, setMounted] = useState(false);
-  useEffect(() => {
-    setMounted(true);
-  }, []);
-
-  if (!mounted) return null;
-
   return (
-    <GoogleReCaptchaProvider reCaptchaKey="6LeOoeUqAAAAACxHiEEOGqsTkvB41eLEo-GJaxGh">
-      <Suspense fallback={<div>Loading...</div>}>
+    <Suspense fallback={<div>Loading...</div>}>
+      <GoogleReCaptchaProvider reCaptchaKey="6LeOoeUqAAAAACxHiEEOGqsTkvB41eLEo-GJaxGh">
         <LoginPage />
-      </Suspense>
-    </GoogleReCaptchaProvider>
+      </GoogleReCaptchaProvider>
+    </Suspense>
   );
 }
 
